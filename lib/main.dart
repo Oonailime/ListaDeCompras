81135fe0ca5d4c707b8195681913cb0c2d6223cf
davisouzak <juliejonatasaraujo@gmail.com>
2024-06-19 02:20:45 -0300

    add categorias

--- lib/main.dart
+++ lib/main.dart
@@ -1,233 +1,163 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Lista de compras",
-    theme: ThemeData(
-      brightness: Brightness.light,
-    ),
-    home: HomePage(),
-  ));
+  runApp(const MyApp());
 }
 
-class Produto {
-  double preco;
-  String nomeProduto;
-  int quantidade;
+final List<String> _categorias = [
+  'Ferramentas',
+  'Comida',
+  'Eletronicos',
+  'Limpeza',
+  'Jogos',
+  'Bebidas'
+];
 
-  Produto({this.nomeProduto = '', this.preco = 0.0, this.quantidade = 0});
-}
-
-double TotalPreco(List<Produto> produtos) {
-  double soma = 0;
-
-  for (var produto in produtos) {
-    soma += produto.preco*produto.quantidade;
-  }
-  return soma;
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<Produto> _compras = [];
-  double _totalPreco = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadCompras(); // Carrega os dados ao iniciar a tela
-    
-  }
-
-  // Método para carregar os dados salvos
-  void _loadCompras() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String>? produtosSalvos = prefs.getStringList('compras');
-    if (produtosSalvos != null) {
-      setState(() {
-        _compras = produtosSalvos
-            .map((produtoString) {
-              List<String> dados = produtoString.split(':');
-              return Produto(
-                nomeProduto: dados[0],
-                preco: double.parse(dados[1]),
-                quantidade: int.parse(dados[1])
-              );
-            })
-            .toList();
-        _totalPreco = TotalPreco(_compras);
-      });
-    }
-  }
-
-  // Método para salvar os dados
-  void _saveCompras() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> produtosParaSalvar = _compras
-        .map((produto) => "${produto.nomeProduto}:${produto.preco.toString()}:${produto.quantidade.toString()}")
-        .toList();
-    await prefs.setStringList('compras', produtosParaSalvar);
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFd2f8d6),
-      appBar: AppBar(
-        backgroundColor: Color(0xFF11e333),
-        title: const Text(
-          'Lista de compras',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Lista de Compras',
+      theme: ThemeData(
+        useMaterial3: true,
       ),
-      body: ListView.builder(
-        itemCount: _compras.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Dismissible(
-            key: Key(_compras[index].nomeProduto),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: ListTile(
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('${_compras[index].nomeProduto}'),
-                    Text('\$ ${_compras[index].preco.toStringAsFixed(2)}'),
-                    Text(' ${_compras[index].quantidade.toStringAsFixed(0)}'),
-                  ],
-                ),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete),
-                  color: Colors.red,
-                  onPressed: () {
-                    setState(() {
-                      _compras.removeAt(index);
-                      _totalPreco = TotalPreco(_compras);
-                      _saveCompras(); // Salva os dados após remover um item
-                    });
-                  },
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xff11e333),
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              Produto novoProduto = Produto(); // Criando um novo produto vazio
+      home: const MyHomePage(),
+    );
+  }
+}
 
-              return AlertDialog(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                title: Text("Adicione um produto"),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    SizedBox(height: 12),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Digite o NOME do produto',
-                      ),
-                      onChanged: (String value) {
-                        novoProduto.nomeProduto = value; // Atualiza o nome do produto
-                      },
-                    ),
-                    SizedBox(height: 12),
-                    SizedBox(
-                      width: 200,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Digite o PREÇO',
-                        ),
-                        keyboardType:
-                            TextInputType.numberWithOptions(decimal: true),
-                        onChanged: (String value) {
-                          novoProduto.preco =
-                              double.tryParse(value) ?? 0.0; // Atualiza o preço do produto
-                        },
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    SizedBox(
-                      width: 200,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Digite a QUANTIDADE',
-                        ),
-                        
-                        onChanged: (String value) {
-                          novoProduto.quantidade =
-                              int.tryParse(value) ?? 0; // Atualiza o preço do produto
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _compras.add(novoProduto); // Adiciona o novo produto à lista
-                        _totalPreco = TotalPreco(_compras); // Atualiza o preço total da lista
-                        _saveCompras(); // Salva os dados após adicionar um item
-                      });
-                      Navigator.of(context).pop();
-                    },
-                    
-                    child: Text('Adicionar'),
-                  )
-                ],
-              );
-            },
-          );
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-      ),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.all(16),
-        color: Colors.grey[800],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  get _quantidadeController => null;
+
+  get _valorController => null;
+
+  @override
+  Widget build(BuildContext context) {
+    var produtoController;
+    return Scaffold(
+      backgroundColor: const Color(0xffD2F8D6),
+      appBar: AppBar(
+        backgroundColor: const Color(0XFF11E333),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Text(
-              'Total:',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+            Icon(
+              Icons.cancel_outlined,
+              color: Colors.white,
             ),
             Text(
-              'R\$ ${_totalPreco.toStringAsFixed(2)}',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+              'Cancelar',
+              style: TextStyle(color: Colors.white),
             ),
           ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              const Text(
+                'Novo produto',
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: produtoController,
+                decoration: InputDecoration(
+                    labelText: 'Nome do poduto',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    filled: true,
+                    fillColor: Colors.white),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _quantidadeController,
+                decoration: InputDecoration(
+                    labelText: 'Quantidade',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    filled: true,
+                    fillColor: Colors.white),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _valorController,
+                decoration: InputDecoration(
+                    labelText: 'Valor(Unidade)',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    filled: true,
+                    fillColor: Colors.white),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const DropdownButtonWidget(
+                categorias: [],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DropdownButtonWidget extends StatefulWidget {
+  final List<String> categorias;
+
+  const DropdownButtonWidget({super.key, required this.categorias});
+
+  @override
+  _DropdownButtonWidgetState createState() => _DropdownButtonWidgetState();
+}
+
+class _DropdownButtonWidgetState extends State<DropdownButtonWidget> {
+  String dropdownValue = 'Ferramentas'; // Valor inicial do dropdown
+
+  @override
+  Widget build(BuildContext context) {
+    return InputDecorator(
+      decoration: InputDecoration(
+        labelText: 'Categoria',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        filled: true,
+        fillColor: Colors.white,
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          icon: const Icon(Icons.arrow_drop_down),
+          iconSize: 24,
+          elevation: 16,
+          style: const TextStyle(color: Colors.deepPurple),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue!;
+            });
+          },
+          items: _categorias.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
         ),
       ),
     );
