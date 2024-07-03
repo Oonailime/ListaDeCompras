44983c88b3212dec7279b6be38fb58aed22b9896
Oonailime <emiliano.guimaraes@ufba.br>
2024-07-03 15:06:38 -0300

    Arrumei o bug de cortar sombras em resoluções baixas

--- lib/app/features/pages/login_page.dart
+++ lib/app/features/pages/login_page.dart
@@ -73,91 +73,86 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 const SizedBox(height: 30.0),
                 Container(
-                  padding: EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
+                  margin: EdgeInsets.all(16.0), // Adiciona espaçamento externo
+                  child: Material(
+                    elevation: 5, // Sombra
                     borderRadius: BorderRadius.circular(15.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 20.0),
-                      Container(
-                        constraints: BoxConstraints(maxWidth: 400),
-                        child: TextField(
-                          controller: _usernameController,
-                          decoration: InputDecoration(
-                            labelText: 'Usuário',
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.person),
-                          ),
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                      Container(
-                        constraints: BoxConstraints(maxWidth: 400),
-                        child: TextField(
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            labelText: 'Senha',
-                            border: OutlineInputBorder(),
-                            prefixIcon: GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  _isPasswordVisible = !_isPasswordVisible;
-                                });
-                              },
-                              child: Icon(
-                                _isPasswordVisible
-                                    ? Icons.lock_open
-                                    : Icons.lock,
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0), // Adiciona espaçamento interno
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20.0),
+                          Container(
+                            constraints: BoxConstraints(maxWidth: 400),
+                            child: TextField(
+                              controller: _usernameController,
+                              decoration: InputDecoration(
+                                labelText: 'Usuário',
+                                border: OutlineInputBorder(),
+                                prefixIcon: Icon(Icons.person),
                               ),
+                              textInputAction: TextInputAction.next,
                             ),
                           ),
-                          textInputAction: TextInputAction.done,
-                          obscureText: !_isPasswordVisible,
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                      ElevatedButton(
-                        onPressed: _login,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF11E333),
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 50, vertical: 12),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
+                          SizedBox(height: 20.0),
+                          Container(
+                            constraints: BoxConstraints(maxWidth: 400),
+                            child: TextField(
+                              controller: _passwordController,
+                              decoration: InputDecoration(
+                                labelText: 'Senha',
+                                border: OutlineInputBorder(),
+                                prefixIcon: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      _isPasswordVisible = !_isPasswordVisible;
+                                    });
+                                  },
+                                  child: Icon(
+                                    _isPasswordVisible
+                                        ? Icons.lock_open
+                                        : Icons.lock,
+                                  ),
+                                ),
+                              ),
+                              textInputAction: TextInputAction.done,
+                              obscureText: !_isPasswordVisible,
+                            ),
                           ),
-                        ),
-                        child: Text(
-                          'Entrar',
-                          style: TextStyle(fontSize: 18, color: Colors.black),
-                        ),
-                      ),
-                      SizedBox(height: 10.0),
-                      TextButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return CreateUserPopup(); // Mostra o popup de criação de usuário
+                          SizedBox(height: 20.0),
+                          ElevatedButton(
+                            onPressed: _login,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF11E333),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                            child: Text(
+                              'Entrar',
+                              style: TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          TextButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return CreateUserPopup(); // Mostra o popup de criação de usuário
+                                },
+                              );
                             },
-                          );
-                        },
-                        child: Text(
-                          'Cadastre-se',
-                          style: TextStyle(fontSize: 16, color: Colors.black87),
-                        ),
+                            child: Text(
+                              'Cadastre-se',
+                              style: TextStyle(fontSize: 16, color: Colors.black87),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ],
@@ -170,10 +165,23 @@ class _LoginPageState extends State<LoginPage> {
         child: Container(
           height: 56.0,
           alignment: Alignment.center,
-          child: Text(
-            'Autores do Projeto: Álvaro Souza, Emiliano Calado, Gabriela Rappel, Lucas Oitabem',
-            style: TextStyle(color: Colors.black, fontSize: 12),
-            textAlign: TextAlign.center,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 12),
+                Text(
+                  'Criador: Emiliano Calado',
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Contato: emiliano.jgac@gmail.com',
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       ),
